2d5bbdeefa0e8069ebdd3b9517d11d5bc649cb53
Aleksey Solovev <dr.nightingale@mail.ru>
2018-02-28 01:39:44 +0800

    cancer-catalog additional artciles fix

--- nenaprasno.ru/public/bitrix/templates/media/components/bitrix/news.detail/media-cancer-catalog/result_modifier.php
+++ nenaprasno.ru/public/bitrix/templates/media/components/bitrix/news.detail/media-cancer-catalog/result_modifier.php
@@ -1,7 +1,7 @@
 <?
 	CModule::IncludeModule('iblock');
 	$arSelect = Array("ID", "IBLOCK_ID", "NAME", "IBLOCK_SECTION_ID", "DETAIL_PAGE_URL", "PROPERTY_*");
-	$arFilter = Array("IBLOCK_ID"=>MEDIA_ADDITIONAL_ARTICLES_IBLOCK, "ACTIVE"=>"Y", 'PROPERTY_DESEASE' => $arResult['NAME']);
+	$arFilter = Array("IBLOCK_ID"=>MEDIA_ADDITIONAL_ARTICLES_IBLOCK, "ACTIVE"=>"Y", 'PROPERTY_DISEASE' => $arResult['NAME']);
 	$res = CIBlockElement::GetList(Array(), $arFilter, false, Array("nPageSize"=>50), $arSelect);
 	while($ob = $res->GetNextElement()){
 		$arFields = $ob->GetFields();
