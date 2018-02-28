658d4568228567ac571d5df75129d063fc8c210e
Aleksey Solovev <dr.nightingale@mail.ru>
2018-02-28 23:27:32 +0800

    Nenaprasno.ru: donate form target ''prosvetitelstvo' delete

--- nenaprasno.ru/public/bitrix/templates/nenaprasno/components/bitrix/news.list/mainsite-projects-select/template.php
+++ nenaprasno.ru/public/bitrix/templates/nenaprasno/components/bitrix/news.list/mainsite-projects-select/template.php
@@ -1,7 +1,6 @@
 <select name="donate-project">
     <option value="common">Общее пожертвование</option>
     <option value="common">Поддержка работы фонда</option>
-    <option value="common">Просветительство</option>
     <? foreach ($arResult['ITEMS'] as $item): ?>
       <? if ($item['CODE'] == $arParams['OPENED']): ?>
         <option selected value="<?=$item['ID']?>"><?=$item['NAME']?></option>
