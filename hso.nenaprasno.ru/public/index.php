a0966c7b1c5ddda17f7d5a84bcf2f6e12dd01183
Artem Garnyshev <is.perfect.possible@gmail.com>
2018-03-15 17:59:51 +0700

    Mainpage content moved into include files

--- hso.nenaprasno.ru/public/index.php
+++ hso.nenaprasno.ru/public/index.php
@@ -11,211 +11,46 @@ $APPLICATION->SetTitle("Главная");
         <div class="wrapper">
             <div class="wrapper-small">
                 <div class="homepage-welcome-title">
-                    Высшая школа онкологии &mdash; это грантовый проект Фонда профилактики рака «Не напрасно», который реализуется совместно с НМИЦ Онкологии им. Н.Н. Петрова в Санкт-Петербурге.
+                    <? $APPLICATION->IncludeFile('/include/mainpage/homepage-welcome-title.php'); ?>
                 </div>
 
                 <div class="homepage-welcome-description">
-                    <div class="homepage-welcome-description-title tt-uppercase">
-                        О проекте
-                    </div>
-
-                    Проект был создан в 2015 году как альтернативный формат поступления в ординатуру одного из лучших онкологических институтов России. Со временем ВШО превратилась в полноценный образовательный проект, который гарантирует грант на оплату обучения в ординатуре по онкологии, ежемесячную стипендию и дополнительную образовательную программу с уникальным набором преподавателей и дисциплин.
+                    <? $APPLICATION->IncludeFile('/include/mainpage/homepage-welcome-description.php'); ?>
                 </div>
 
                 <div class="homepage-welcome-buttons">
                     <a href="#" class="button button-orange button-round" style="width: 220px;">Принять участие</a>
-
-                    <span style="color:#666666">Приём заявок заканчивается</span> 14 апреля 2018 года в 23:59
+                    <? $APPLICATION->IncludeFile('/include/mainpage/homepage-welcome-buttons.php'); ?>
                 </div>
             </div>
         </div>
     </section>
     <section class="homepage-grants b-light-orange section-padding-top section-padding-bottom">
         <div class="wrapper-small">
-            <div class="homepage-grants-title">
-                Грант по следующим специальностям
-            </div>
-
-            <div class="row">
-                <div class="col-xs-6 col-sm-3">
-                    <div class="homepage-grants-item">
-                        <div class="homepage-grants-item-circle">
-                            1
-                        </div>
-                        Онколог
-                    </div>
-                </div>
-                <div class="col-xs-6 col-sm-3">
-                    <div class="homepage-grants-item">
-                        <div class="homepage-grants-item-circle">
-                            2
-                        </div>
-                        Детский онколог
-                    </div>
-                </div>
-                <div class="col-xs-6 col-sm-3">
-                    <div class="homepage-grants-item">
-                        <div class="homepage-grants-item-circle">
-                            3
-                        </div>
-                        Радиолог
-                    </div>
-                </div>
-                <div class="col-xs-6 col-sm-3">
-                    <div class="homepage-grants-item">
-                        <div class="homepage-grants-item-circle">
-                            4
-                        </div>
-                        Патоморфолог
-                    </div>
-                </div>
-            </div>
+            <? $APPLICATION->IncludeFile('/include/mainpage/homepage-grants.php'); ?>
         </div>
     </section>
 
     <section class="homepage-about-hso section-padding-top section-padding-bottom">
         <div class="wrapper-small">
-            <div class="homepage-about-hso-title text-center">
-                Программа ВШО
-            </div>
-
-            <img src="images/bg-about-hso.jpg" alt="" style="margin-bottom: 30px;">
-
-            <p>
-                Тщательно подобранный и регулярно расширяемый пакет курсов по дисциплинам и направлениям, которые пока не присутствуют в программах стандартных онкологических ординатур.
-            </p>
-
-            <p>
-                Все врачи, которых Фонд профилактики рака готовит в рамках программы - специалисты, обученные по международным стандартам, активные и способные создавать точки роста для современной работы онкослужб и медицинских центров, настоящие лидеры для онкологии России.
-            </p>
-
-            <p>
-                Все подробности о конкурсе, проекте и его участниках вы можете узнать в официальном сообществе проекта в vk.com
-            </p>
-
-            <div class="homepage-about-hso-notice">
-                Победители конкурса получают грант на обучение в ординатуре по онкологии на 2018-2020 год*, ежемесячную стипендию 20 000 рублей и обучение по специальной программе ВШО.
-            </div>
+            <? $APPLICATION->IncludeFile('/include/mainpage/homepage-about-hso.php'); ?>
         </div>
     </section>
 
     <section class="homepage-hso-is b-light-orange section-padding-top section-padding-bottom">
-        <div class="homepage-hso-is-title text-center">
-            Высшая школа онкологии &mdash; это:
-        </div>
-
-        <div class="wrapper">
-            <div class="row">
-                <div class="col-xs-6 col-sm-3">
-                    <div class="homepage-hso-is-item">
-                        <div class="homepage-hso-is-item-circle">
-                            1
-                        </div>
-                        Специальная программа дополнительного обучения, состоящая из курсов, которых нет в стандартном пакете ординатуры в России;
-                    </div>
-                </div>
-                <div class="col-xs-6 col-sm-3">
-                    <div class="homepage-hso-is-item">
-                        <div class="homepage-hso-is-item-circle">
-                            2
-                        </div>
-                        Новейшая международная литература и научные материалы по онкологии и патоморфологии;
-                    </div>
-                </div>
-                <div class="col-xs-6 col-sm-3">
-                    <div class="homepage-hso-is-item">
-                        <div class="homepage-hso-is-item-circle">
-                            3
-                        </div>
-                        Журнальные клубы, мастер-классы и неформальные встречи с российскими и зарубежными  специалистами по онкологии;
-                    </div>
-                </div>
-                <div class="col-xs-6 col-sm-3">
-                    <div class="homepage-hso-is-item">
-                        <div class="homepage-hso-is-item-circle">
-                            4
-                        </div>
-                        Стажировки в лучших центрах России;
-                    </div>
-                </div>
-                <div class="col-xs-6 col-sm-3">
-                    <div class="homepage-hso-is-item">
-                        <div class="homepage-hso-is-item-circle">
-                            5
-                        </div>
-                        Сообщество единомышленников, которые стремятся изменить ситуацию, сделать отечественную онкологию лучше, развить ее возможности;
-                    </div>
-                </div>
-                <div class="col-xs-6 col-sm-3">
-                    <div class="homepage-hso-is-item">
-                        <div class="homepage-hso-is-item-circle">
-                            6
-                        </div>
-                        Еженедельные встречи в командах для самостоятельной работы, кружки по химиотерапии, патоморфологии и отдельные занятия по хирургии;
-                    </div>
-                </div>
-                <div class="col-xs-6 col-sm-3">
-                    <div class="homepage-hso-is-item">
-                        <div class="homepage-hso-is-item-circle">
-                            7
-                        </div>
-                        Атмосфера, которой нет ни в одной ординатуре страны, основанная на ценностях взаимопомощи и профессионализма;
-                    </div>
-                </div>
-                <div class="col-xs-6 col-sm-3">
-                    <div class="homepage-hso-is-item">
-                        <div class="homepage-hso-is-item-circle">
-                            8
-                        </div>
-                        Помощь в трудоустройстве после окончания ординатуры и дополнительной программы обучения.
-                    </div>
-                </div>
-            </div>
-        </div>
+        <? $APPLICATION->IncludeFile('/include/mainpage/homepage-hso-is.php'); ?>
     </section>
 
     <section class="homepage-additional-program section-padding-top section-padding-bottom">
         <div class="wrapper">
-            <div class="homepage-additional-program-title">
-                Дополнительная программа обучения включает в себя следующие курсы:
-            </div>
-            <div class="row">
-                <div class="col-xs-12 col-sm-6">
-                    <ul class="ul-box">
-                        <li>Принятие клинических решений в онкологии;</li>
-                        <li>Методология критического чтения и интерпретации научных исследований;</li>
-                        <li>Основы эффективного общения с пациентом;</li>
-                        <li>Основы организации работы команд в онкологии;</li>
-                        <li>Базовая биостатистика;</li>
-                        <li>End of life care;</li>
-                    </ul>
-                </div>
-                <div class="col-xs-12 col-sm-6">
-                    <ul class="ul-box">
-                        <li>Основы онкогематологии</li>
-                        <li>Основы онкоэпидемиологии и анализ выживаемости</li>
-                        <li>Молекулярная онкология и опухолевые синдромы</li>
-                        <li>Основы патоморфологии</li>
-                        <li>Английский язык для врачей и исследователей</li>
-                        <li>Основы академического письма</li>
-                        <li>Основы профессиональной и массовой коммуникации</li>
-                    </ul>
-                </div>
-            </div>
+            <? $APPLICATION->IncludeFile('/include/mainpage/homepage-additional-program.php'); ?>
         </div>
     </section>
 
     <div class="section-gray section-padding-top section-padding-bottom">
         <div class="wrapper">
             <div class="competition-conditions">
-
-                <?$APPLICATION->IncludeFile(
-                  SITE_DIR."include/tours.php",
-                  Array(),
-                  Array("MODE"=>"html")
-                  );
-                ?>
-
+                <? $APPLICATION->IncludeFile('/include/mainpage/tours.php'); ?>
             </div>
         </div>
     </div>
@@ -261,18 +96,7 @@ $APPLICATION->SetTitle("Главная");
 
             <div class="row">
                 <div class="homepage-about-fund-desc">
-                    <p>
-                        Фонд профилактики рака &mdash; независимая некоммерческая организация, которая реализует
-                        благотворительные проекты в сфере профилактики рака на всей территории России.
-                    </p>
-
-                    <p>
-                        <b>Миссия Фонда:</b> снизить смертность от рака в России.
-                    </p>
-
-                    <p>
-                        Фонд обладает экспертизой в вопросах: онкоэпидемиологии, скрининга, медицинского образования, организации здравоохранения и оценки качества работы онкологических служб.
-                    </p>
+                    <? $APPLICATION->IncludeFile('/include/mainpage/homepage-about-fund-desc.php'); ?>
                 </div>
                 <div class="homepage-about-fund-logo">
                     <img src="images/nenaprasno-logo.svg" alt="Фонд профилактики рака «Не напрасно»" width="273">
